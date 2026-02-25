4fe42e99f49cd365b43e3274efbddf3665a4e762
Jasmine Carrion <152462643+jasmine0513@users.noreply.github.com>
2026-02-25 13:21:51 -0500

    Add files via upload

---
+++ turning turtle.py
@@ -0,0 +1,15 @@
+
+"""
+Jasmine Carrion
+jasmine.carrion73@myhunter.cuny.edu
+"""
+
+import turtle
+
+t = turtle.Turtle()
+
+for JAS in range(5):
+    D = int (input("Enter #: "))
+    t.left(D)
+    t.forward(100)
+turtle.done()
